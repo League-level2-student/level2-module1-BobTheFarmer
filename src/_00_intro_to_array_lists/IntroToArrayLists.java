1d67660090c21829722fc602fa04f37a5ad99dc8
league <league@imac-12.attlocal.net>
2021-08-06 17:00:06 -0700

    Started intro to array lists

--- src/_00_intro_to_array_lists/IntroToArrayLists.java
+++ src/_00_intro_to_array_lists/IntroToArrayLists.java
@@ -1,18 +1,33 @@
 package _00_intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList (java.util) class
-
+		ArrayList<String> array = new ArrayList<String>();
 		//2. Add five Strings to your list
-		
+		array.add("dogs");
+		array.add("dogs2");
+		array.add("dogs3");
+		array.add("dogssdafhdao");
+		array.add("edsafd");
 		//3. Print all the Strings using a standard for-loop
-		
+		for(int i = 0; i<5; i++) {
+			System.out.println(array.get(i));
+		}
 		//4. Print all the Strings using a for-each loop
-		
+		for (int i = 0; i < array.size(); i++) {
+			System.out.println(array.get(i));
+		}
 		//5. Print only the even numbered elements in the list.
-		
+		System.out.println("Evens: ");
+		for (int i = 0; i < array.size(); i++) {
+			if(i%2==0) {
+			System.out.println(array.get(i));
+			}
+		}
 		//6. Print all the Strings in reverse order.
 		
 		//7. Print only the Strings that have the letter 'e' in them.
